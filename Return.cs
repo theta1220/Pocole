22f3cc99a98fa6472318859ac965144821571bac
furukawa <nkc.takashi.furukawa@gmail.com>
2020-01-12 17:39:28 +0900

    return単体でも実行可能になった

--- Return.cs
+++ Return.cs
@@ -6,7 +6,14 @@ namespace Pocole
         public string Formula { get; private set; }
         public Return(Runnable parent, string source) : base(parent, source)
         {
-            Formula = Util.String.SplitOnce(source, ' ')[1];
+            var split = Util.String.SplitOnce(source, ' ');
+            if (split.Length < 2)
+            {
+                // NOTE: "return;" としか書かれていない場合は nullを返すことにしておく
+                Formula = "null";
+                return;
+            }
+            Formula = split[1];
         }
 
         protected override void Run()
